8308819be92466f6724cf6943a771febaf2b98d9
Gapper1000 <143434072+Gapper1000@users.noreply.github.com>
2023-10-30 22:56:02 -0400

    Fixed the formatting of my code

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,16 +1,26 @@
 from getpass import getpass
 
 import os
+import json
+from pprint import pprint
+from statistics import mean
+
 from dotenv import load_dotenv
+import requests
+from plotly.express import line
+
 
 load_dotenv()
 
-API_KEY = os.getenv("Please input your AlphaVantage API Key: ")
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+print(API_KEY)
+
+#breakpoint()
+
+#quit()
 
 
-import requests
-import json
-from pprint import pprint
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
@@ -39,7 +49,6 @@ print(f"{data[0]['value']}%", "as of", data[0]["date"])
 # What is the average unemployment rate for all months during this calendar year?
 # ... How many months does this cover?
 
-from statistics import mean
 
 this_year = [d for d in data if "2023-" in d["date"]]
 
@@ -54,7 +63,6 @@ print("NO MONTHS:", len(this_year))
 #
 # Plot a line chart of unemployment rates over time.
 
-from plotly.express import line
 
 dates = [d["date"] for d in data]
 rates = [float(d["value"]) for d in data]
